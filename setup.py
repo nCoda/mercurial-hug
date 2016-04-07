735bc88ac89524e4a7c03e711c61a22c1c993eb7
Christopher Antila <christopher@antila.ca>
2016-04-07 00:25:01 -0400

    Remove irrelevant metadata from setup.py
    We can't support Py3 yet because Mercurial won't install there. Ho hum.

--- setup.py
+++ setup.py
@@ -69,13 +69,9 @@ setup(
     classifiers =[
         'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
-        'Topic :: Software Development :: Interpreters',
     ],
 )
